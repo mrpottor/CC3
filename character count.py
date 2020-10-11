441f37fdfb963ff5488cd88417659bc8c3e75d39
Aditya <adityar610@gmail.com>
2020-10-11 10:11:32 +0530

    minute modification

--- character count.py
+++ character count.py
@@ -1,12 +1,12 @@
 T = int(input("how many tests you want to bro \n"))
 # taking input for no of test cases 
-while T > 0: # while loop to iterate through tests
-    word = input("enter the word bro:-\n")# TAKING word input 
-    word = list(word) # converting words to list
-    count = dict() # declaring an empty dictionary
+while T > 0:  # while loop to iterate through tests
+    word = input("enter the word bro:-\n")  # TAKING word input
+    word = list(word)  # converting words to list
+    count = dict()  # declaring an empty dictionary
     for i in word:
-        count[ i ] = count.get(i , 0) + 1
+        count[i] = count.get(i, 0) + 1
     for i in count:
-        print(f"{i}-{count.get(i , 0)}" , end=' ')
+        print(f"{i}-{count.get(i , 0)}", end=' ')
     print("")
     T -= 1
