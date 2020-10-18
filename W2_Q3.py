dae700b7171a7e4fbe08e6e79e10d0d615e43875
Aditya <adityar610@gmail.com>
2020-10-18 18:16:16 +0530

    weak 2 left task

---
+++ W2_Q3.py
@@ -0,0 +1,7 @@
+from math import sqrt
+from _functools import reduce
+def factors(n):
+        step = 2 if n%2 else 1
+        return set(reduce(list.__add__,
+                    ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
+                    
