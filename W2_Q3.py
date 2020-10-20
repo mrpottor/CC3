586acf19113c571f0636edbce88643da7d55b84c
Aditya <adityar610@gmail.com>
2020-10-20 17:55:35 +0530

    added weak 3 left task

--- W2_Q3.py
+++ W2_Q3.py
@@ -4,4 +4,9 @@ def factors(n):
         step = 2 if n%2 else 1
         return set(reduce(list.__add__,
                     ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
-                    
+
+t = int(input('no of tests to run -'))
+for i in range(t):
+    n = int(input('number n - '))
+
+    print(factors(n))
