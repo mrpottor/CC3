c71b26b955bea1f25c317e9c23fbb088b5cce0c2
Aditya <adityar610@gmail.com>
2020-10-24 21:42:25 +0530

    .

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ import re
 T = int(input('no of tests\n'))
 for i in range(T):
 
-    s , p = (str(x) for x in input('enter string and password').split())
+    s , p = (str(x) for x in input('enter string and password :-').split())
     if len(p) == 2*(len(s)):
         if re.search(s,p):
             print("possible")
